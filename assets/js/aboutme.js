4fe65055f5920d75a22235fc885cae13f3a446de
marick-py <ricomari2006@gmail.com>
2023-08-22 10:53:38 +0200

    uploading code

---
+++ assets/js/aboutme.js
@@ -0,0 +1,169 @@
+const nodesContainer = document.getElementById('Nodes');
+const linesContainer = document.getElementById('Lines');
+
+let jsonData;
+
+let mouseX;
+let mouseY;
+
+const y_space = 150;
+
+fetch('assets/json/randomAboutMe.json')
+.then(response => response.json())
+.then(data => {
+    jsonData = data;
+    updateGraph();
+})
+.catch(error => {console.error('Error loading JSON:', error);});
+
+let currentPath = ['About Me', 'Life and Coding', 'Coding Adventures', 'Web Wizardry', 'Digital Playground', 'Godot Exploration'];
+
+function updateGraph() {
+    let nodeElements = [];
+    let lineElements = [];
+
+    function removeAllNodes(container) {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+    }
+
+    function createNode(path_to_this_node, x, y, max_w, in_selected_path) {
+        const node = document.createElement('div');
+        if (in_selected_path) {
+            node.className = 'node selected';
+        } else {
+            node.className = 'node';
+        }
+        node.style.left = `${x}px`;
+        node.style.top = `${y}px`;
+        node.style.maxWidth = `${max_w}px`;
+        node.path_to_this_node = path_to_this_node;
+        node.textContent = path_to_this_node[path_to_this_node.length-1];
+        node.x = x;
+        node.y = y;
+        return node;
+    }
+
+    function createLine(p1, p2, minDelta) {
+        const line = document.createElement('div');
+        line.p1 = p1;
+        line.p2 = p2;
+        const delta = p2.x - p1.x;
+        line.style.transformOrigin = `0px 0px`;
+
+        if (delta == 0) {
+            line.className = 'vertical-line';
+
+            line.style.width = `${Math.abs(p2.y - p1.y)}px`;
+            line.style.height = '2px';
+            line.style.left = `${p1.x}px`;
+            line.style.top = `${p1.y}px`;
+            line.style.transform = `rotate(${Math.PI/2}rad)`;
+        
+        } else {
+            line.style.height = '2px';
+            line.style.left = `${p1.x}px`;
+            line.style.top = `${p1.y}px`;
+            
+            const absDelta = Math.abs(delta);
+            if (minDelta < y_space) {
+                line.className = 'advanced-short-line';
+                line.style.setProperty('--after-before-size', `${minDelta/2}px`);
+                line.style.setProperty('--after-before-size-2', `${y_space/2}px`);
+                line.style.width = `${absDelta - minDelta}px`;
+                line.style.transform = `scaleX(${(delta > 0) ? 1 : -1}) translate(${minDelta/2}px, ${y_space/2}px)`;
+            } else {
+                line.className = 'advanced-line';
+                line.style.width = `${absDelta - y_space}px`;
+                line.style.setProperty('--after-before-size', `${y_space/2}px`);
+                line.style.transform = `scaleX(${(delta > 0) ? 1 : -1}) translate(${y_space/2}px, ${y_space/2}px)`;
+            }
+        }
+        return line;
+    }
+
+    function renderPath(path) {
+        let y = 100;
+        let x_space;
+        let next_nodes = jsonData;
+        let first_element;
+
+        let last_row_of_lines = [];
+
+        for (let i = 0; i < path.length+1; i++) {
+            if (i == 0) {
+                next_nodes = next_nodes;
+            } else {
+                next_nodes = next_nodes[path[i-1]];
+            }
+            if (typeof next_nodes === 'object') {
+                x_space = window.innerWidth / (Object.keys(next_nodes).length + 1);
+                Object.entries(next_nodes).forEach(([key, value], index) => {
+                    let nx = x_space * (index + 1);
+                    let ny = y + y_space * i;
+                    const node = createNode(path.slice(0,i).concat([key]), nx, ny, x_space, path.includes(key));
+                    if (path.includes(key)) {
+                        if (i != 0) {
+                            lineElements.push(i === 1 ? [first_element, node] : [lineElements[lineElements.length - 1][1], node]);
+                        } else {
+                            node.className = "node first-element"
+                           first_element = node;
+                        }
+                    }
+                    if (i == path.length) {
+                        last_row_of_lines.push(i === 1 ? [first_element, node] : [lineElements[lineElements.length - 1][1], node]);
+                        node.className = "node last-row";
+                    }
+                    nodeElements.push(node);
+                });
+            } else {
+                const node = createNode([next_nodes], window.innerWidth / 2, y + y_space * i, window.innerWidth, true);
+                last_row_of_lines.push(i === 1 ? [first_element, node] : [lineElements[lineElements.length - 1][1], node]);
+                nodeElements.push(node);
+                node.className = "node description";
+                node.id = "description";
+            }
+        }
+        lineElements = lineElements.concat(last_row_of_lines);
+    }
+
+    removeAllNodes(nodesContainer);
+    removeAllNodes(linesContainer);
+
+    renderPath(currentPath);
+
+    const get_deltas = ps => Math.abs(ps[0].x - ps[1].x);
+    const minValue = Math.min(...lineElements.map(get_deltas).filter(d => d !== 0));
+
+    nodeElements.forEach(node => nodesContainer.appendChild(node));
+    lineElements.forEach(line => linesContainer.appendChild(createLine(...line, minValue)));
+
+
+    const y_scroll = (window.scrollY || window.pageYOffset);
+    const get_y_max = node => node.getBoundingClientRect().bottom + y_scroll;
+
+    const maxHeight = Math.max(...nodeElements.map(get_y_max));
+    const separator = document.getElementById("content-spacer");
+
+    let height = maxHeight - separator.getBoundingClientRect().top - y_scroll;
+
+    separator.style.height = `${Math.max(height, 0)}px`;
+}
+
+document.addEventListener('mousemove', event => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+});
+
+document.addEventListener('click', event => {
+    if (event.target.classList.contains('node') && !event.target.classList.contains("description")) {
+        currentPath = event.target.path_to_this_node;
+        updateGraph();
+    }
+});
+
+window.addEventListener('resize', () => {
+    // Call a function to update nodes' positions here
+    updateGraph();
+});
---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+const menuToggles = document.getElementsByClassName('menu-toggle');
+const closeButton = document.getElementById("close-button");
+const globalHeader = document.getElementById('global-header');
+
+const contentSectionCss = document.querySelector('#content');
+
+const homeButtonCss = document.querySelector('.home-button');
+const aboutButtonCss = document.querySelector('.about-button');
+const contactButtonCss = document.querySelector('.contact-button');
+
+const overlayCss = document.querySelector('.overlay');
+const lateralMenuCss = document.querySelector('#menu');
+const closeButtonCss = document.querySelector("#menu .close-button");
+const bannerCss = document.querySelector('.banner');
+
+function closeMenu() {
+	overlayCss.classList.toggle('open');
+	lateralMenuCss.classList.toggle('open');
+	closeButtonCss.classList.toggle("open");
+}
+
+[...menuToggles, closeButton].forEach(menuToggle => {
+	menuToggle.addEventListener('click', () => {
+        closeMenu()
+	});
+});
+
+homeButtonCss.addEventListener('click', function(event) {
+	event.preventDefault();
+	closeMenu()
+	window.scrollTo({
+	  top: 0,
+	  behavior: "smooth"
+	});
+});
+
+contactButtonCss.addEventListener('click', function(event) {
+	event.preventDefault();
+	closeMenu()
+	const documentHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
+	window.scrollTo({
+	  top: documentHeight,
+	  behavior: "smooth"
+	});
+});
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > bannerCss.offsetHeight) {
+		globalHeader.classList.remove('scrolled');
+	} else {
+		globalHeader.classList.add('scrolled');
+    }
+});
