b15eaf9dc508f4c1c059291313f77e89e5016539
marick-py <ricomari2006@gmail.com>
2024-02-25 21:07:13 +0100

    benny

--- assets/js/puffetta.js
+++ assets/js/puffetta.js
@@ -1,20 +1,3 @@
-fetch('assets/json/puffetta.json')
-.then(response => response.json())
-.then(jsonData => {
-  const cascadeListContainer = document.getElementById('cascade-list-container');
-
-  jsonData["benny_types"].forEach((text) => {
-    const listItem = document.createElement('p');
-    listItem.classList.add("benny-type")
-    listItem.textContent = text;
-    cascadeListContainer.appendChild(listItem);
-  });
-
-  const pluribennyTitle = document.getElementById("pluribenny-title");
-  pluribennyTitle.textContent = jsonData["benny_types"].length + " SFUMATURE DI BENNY"
-})
-.catch(error => console.error('Error loading JSON:', error));
-
 function expandDescription() {
     const description = document.getElementById("description");
     const banner_img = document.getElementById("banner-img");
